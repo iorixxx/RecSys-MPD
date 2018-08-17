3f0270e051dfb8473f5dd5436d086ab43c412c50
aliyurekli <yurekli.ali@gmail.com>
2018-08-17 11:35:57 +0000

    index shell script is updated for new server environment

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # remove previously created index
-rm -rf /home/recsys2018/apc/MPD.index
+rm -rf /mnt/MPD.index
 
 # go to source code directory, update project, build project
 cd /home/recsys2018/apc/RecSys-MPD
@@ -9,4 +9,4 @@ git pull
 mvn clean package
 
 # run indexer application
-java -server -Xms10g -Xmx25g -jar target/mpd.jar /home/recsys2018/apc/MPD.index /home/recsys2018/apc/dataset/mpd/data
+java -server -Xms20g -Xmx50g -jar target/mpd.jar /mnt/MPD.index /home/recsys2018/apc/dataset/mpd/data
