a043e6636cade652ba5a065caf56b35d9f0f32fd
aliyurekli <yurekli.ali@gmail.com>
2018-08-29 13:51:43 +0300

    search shell script is updated for new server environment

--- search.sh
+++ search.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-cd /mnt/recSys/RecSys-MPD
-#git pull
+
+# go to source code directory, update project, build project
+cd /home/recsys2018/apc/RecSys-MPD
+git pull
 mvn clean package
-cd /mnt/recSys/
-java -server -Xms10g -Xmx25g -cp /mnt/recSys/RecSys-MPD/target/mpd.jar edu.anadolu.app.SearchApp /mnt/recSys/MPD.index /mnt/recSys/challenge_set.json dph.csv RECSYS DPH false Mode3
-python verify_submission.py challenge_set.json submission.csv
+
+# run searcher application
+java -server -Xms10g -Xmx20g -cp target/mpd.jar edu.anadolu.app.SearchApp /mnt/MPD.index /home/recsys2018/apc/dataset/test/test-001.json dph.csv RECSYS DPH false Mode3 true
