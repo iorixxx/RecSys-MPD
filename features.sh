5e689e820bf6f2c06488681ef7bacd63526a5de7
aliyurekli <yurekli.ali@gmail.com>
2018-09-27 14:04:27 +0000

    Bash scripts are moved to a single folder

--- features.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd ~/RecSys-MPD
-#git pull
-mvn clean package
-cd ~/
-java -server -Xms5g -Xmx5g -cp ~/RecSys-MPD/target/mpd.jar edu.anadolu.Feature ~/data/
-wc -l Features.txt
--- search.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# go to source code directory, update project, build project
-cd /home/recsys2018/apc/RecSys-MPD
-git pull
-mvn clean package
-
-# run searcher application
-java -server -Xms10g -Xmx20g -cp target/mpd.jar edu.anadolu.app.SearchApp /mnt/MPD.index /home/recsys2018/apc/dataset/test/test-001.json dph.csv DPH false Mode3 true
