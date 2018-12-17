1dffd170e2479d9d36753bf47716ea5cce6da38f
aliyurekli <yurekli.ali@gmail.com>
2018-12-18 00:40:13 +0300

    Top-n selective sampling is updated

--- bash/topn_ltr_sampling.sh
+++ bash/topn_ltr_sampling.sh
@@ -15,6 +15,9 @@ SEARCHFIELD="Track"
 TOPK=200
 SIZES=(50 100 150 200 250)
 
+FEATURES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
+
+
 # go to source folder, pull changes from the repository, and build project
 cd $SRC
 git pull
